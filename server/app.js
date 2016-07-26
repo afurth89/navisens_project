cced96f86b80ac109277d34d32c9dcf22a3d3d2c
Andrew Furth <andrew.furth@gmail.com>
2016-07-26 12:09:46 -0700

    data api route retrieves seed data

--- server/app.js
+++ server/app.js
@@ -8,5 +8,22 @@ const morgan = require('morgan');
 
 const data = require('./routes/data');
 
+app.use('/views', express.static(__dirname + '/../client/views'))
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.use(morgan('tiny'));
+
+app.use('/api/data', data)
+
+app.get('*', (req, res) => {
+  res.sendFile('layout.html', {root: './client/views'})
+})
+
+app.listen(3000, ()=>{
+  console.log('Server listening on port 3000');
+});
 
 module.exports = app;
--- server/routes/data.js
+++ server/routes/data.js
@@ -3,6 +3,14 @@ var express = require('express')
     db = require('../models'),
     Promise = require("bluebird");
 
-
+router.route('/')
+  .get((req, res) => {
+    db.Data.find({})
+      .exec((err, data) => {
+        if (err) throw err;
+        console.log("Returned data: ", data)
+        res.send(data)
+      }) 
+  })
 
 module.exports = router;
